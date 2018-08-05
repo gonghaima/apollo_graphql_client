fb33b69c76232c816e05ce0ab9793ab6a41110c8
steven <gonghaima@hotmail.com>
2018-08-05 21:41:57 +1000

    mutations

--- src/index.js
+++ src/index.js
@@ -2,95 +2,124 @@ import React, { Component } from "react";
 import { render } from "react-dom";
 
 import ApolloClient from "apollo-boost";
-import { ApolloProvider, Query } from "react-apollo";
+import { ApolloProvider, Query, Mutation } from "react-apollo";
 import gql from "graphql-tag";
 
 const client = new ApolloClient({
-  uri: `https://nx9zvp49q7.lp.gql.zone/graphql`
+  uri: "https://8v9r9kpn7q.lp.gql.zone/graphql"
 });
 
-const GET_DOGS = gql`
+const GET_TODOS = gql`
   {
-    dogs {
+    todos {
       id
-      breed
+      type
     }
   }
 `;
 
-const Dogs = ({ onDogSelected }) => (
-  <Query query={GET_DOGS}>
+const UPDATE_TODO = gql`
+  mutation updateTodo($id: String!, $type: String!) {
+    updateTodo(id: $id, type: $type) {
+      id
+      type
+    }
+  }
+`;
+
+const Todos = () => (
+  <Query query={GET_TODOS}>
     {({ loading, error, data }) => {
-      if (loading) return "Loading...";
-      if (error) return `Error! ${error.message}`;
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error :(</p>;
 
-      return (
-        <select name="dog" onChange={onDogSelected}>
-          {data.dogs.map(dog => (
-            <option key={dog.id} value={dog.breed}>
-              {dog.breed}
-            </option>
-          ))}
-        </select>
-      );
+      return data.todos.map(({ id, type }) => {
+        let input;
+
+        return (
+          <Mutation mutation={UPDATE_TODO} key={id}>
+            {(updateTodo, { loading, error }) => (
+              <div>
+                <p>{type}</p>
+                <form
+                  onSubmit={e => {
+                    e.preventDefault();
+                    updateTodo({ variables: { id, type: input.value } });
+
+                    input.value = "";
+                  }}
+                >
+                  <input
+                    ref={node => {
+                      input = node;
+                    }}
+                  />
+                  <button type="submit">Update Todo</button>
+                </form>
+                {loading && <p>Loading...</p>}
+                {error && <p>Error :( Please try again</p>}
+              </div>
+            )}
+          </Mutation>
+        );
+      });
     }}
   </Query>
 );
 
-const GET_DOG_PHOTO = gql`
-  query dog($breed: String!) {
-    dog(breed: $breed) {
+const ADD_TODO = gql`
+  mutation addTodo($type: String!) {
+    addTodo(type: $type) {
       id
-      displayImage
+      type
     }
   }
 `;
 
-const DogPhoto = ({ breed }) => (
-  <Query
-    query={GET_DOG_PHOTO}
-    variables={{ breed }}
-    notifyOnNetworkStatusChange
-    // pollInterval={500}
-  >
-    {({ loading, error, data, refetch, networkStatus }) => {
-      if (networkStatus === 4) return "Refetching!";
-      if (loading) return null;
-      if (error) return `Error!: ${error}`;
+const AddTodo = () => {
+  let input;
 
-      return (
+  return (
+    <Mutation
+      mutation={ADD_TODO}
+      update={(cache, { data: { addTodo } }) => {
+        const { todos } = cache.readQuery({ query: GET_TODOS });
+        cache.writeQuery({
+          query: GET_TODOS,
+          data: { todos: todos.concat([addTodo]) }
+        });
+      }}
+    >
+      {addTodo => (
         <div>
-          <img
-            src={data.dog.displayImage}
-            style={{ height: 100, width: 100 }}
-          />
-          <button onClick={() => refetch()}>Refetch!</button>
+          <form
+            onSubmit={e => {
+              e.preventDefault();
+              addTodo({ variables: { type: input.value } });
+              input.value = "";
+            }}
+          >
+            <input
+              ref={node => {
+                input = node;
+              }}
+            />
+            <button type="submit">Add Todo</button>
+          </form>
         </div>
-      );
-    }}
-  </Query>
+      )}
+    </Mutation>
+  );
+};
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <div>
+      <h2>Building Mutation components 🚀</h2>
+      <AddTodo />
+      <Todos />
+    </div>
+  </ApolloProvider>
 );
-
-class App extends Component {
-  state = { selectedDog: null };
-
-  onDogSelected = ({ target }) => {
-    this.setState(() => ({ selectedDog: target.value }));
-  };
-
-  render() {
-    return (
-      <ApolloProvider client={client}>
-        <div>
-          <h2>Building Query components 🚀</h2>
-          {this.state.selectedDog && (
-            <DogPhoto breed={this.state.selectedDog} />
-          )}
-          <Dogs onDogSelected={this.onDogSelected} />
-        </div>
-      </ApolloProvider>
-    );
-  }
-}
 
 render(<App />, document.getElementById("root"));
